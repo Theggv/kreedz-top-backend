5a148fb28c35113af13d16b8dae4a885bec05cc4
Theggv <m11rrrrr123@gmail.com>
2022-05-01 01:26:21 +0500

    Refactoring

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
-import { setupSwagger } from './util/setupSwagger';
+import { setupSwagger } from './util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
